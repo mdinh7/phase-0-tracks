d999a23185af4780a721e26263a25f1fdf14c0b1
Mendel Dinh <mdinh6@gmail.com>
2017-03-13 02:42:39 -0700

    Add allergy loop, Add end statement

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,7 +3,7 @@ employees = gets.chomp.to_i
 
 until employees == 0
 	puts "What is your name?"
-	name = gets.chomp
+	name = gets.chomp.to_s
 	puts "How old are you?"
 	age = gets.chomp.to_i
 	puts "What year were you born?"
@@ -12,20 +12,32 @@ until employees == 0
 	bread = gets.chomp
 	puts "Would you like to enroll in the company's health insurance?"
 	insurance = gets.chomp
+		loop do
+			puts "Name any allergies"
+			allergy = gets.chomp.to_s
+			if allergy == "done"
+				break
+			elsif allergy == "sunshine"
+				puts "Probably a vampire"
+				break
+			end
+		end
 
-	if  2016 - year.to_i == age || 2017 - year.to_i == age
-		bread = "Yes" || insurance = "Yes"
-		puts "Probably not a vampire"
+	if name = "Drake Cula" || name = "Tu Fang"
+		puts "Definitely a vampire"
 	elsif 2016 - year.to_i != age || 2017 - year.to_i != age
 		bread = "No" || insurance = "No"
 		puts "Probably a vampire"
 	elsif 2016 - year.to_i != age || 2017 - year.to_i != age
 		bread = "No" && insurance = "No"
 		puts "Almost certainly a vampire"
-	elsif name = "Drake Cula" || name = "Tu Fang"
-		puts "Definitely a vampire"
+	elsif 2016 - year.to_i == age || 2017 - year.to_i == age
+		bread = "Yes" || insurance = "Yes"
+		puts "Probably not a vampire"
 	else
 		puts "Results inconclusive"
 	end
+	employees -= 1
 end
-	
+
+puts  "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
