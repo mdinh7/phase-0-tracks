3c90c658b4087d695f8ac0fe6ae1504a81502c9a
Mendel Dinh <mdinh6@gmail.com>
2017-03-20 03:16:52 -0700

    Add nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,20 @@
+restaraunt = { 
+'Location One' => {
+	'Appetizers' => ['Calmari', 'Truffle Fries', 'Mini Lamb Sliders']
+	'Salads' => [ 'Caesar', 'Kale', 'BBQ Chicken', 'Potato']
+	'Main Dishes' => [ 'Kobe Beef Burger', 'Pesto Pasta with Chicken', 'Lobster and Steak', 'Fish and Chips']
+	'Dessert' => ['Gelato', 'Lava Cake', 'Giant Cookie', 'Apple Pie']
+	}
+'Location Two' => {
+	'Appetizers' => ['Calmari', 'Truffle Fries', 'Lettuce Wrap']
+	'Salads' => [ 'Caesar', 'Kale', 'BBQ Chicken', 'Shrimp']
+	'Main Dishes' => [ 'Kobe Beef Burger', 'Alfredo Pasta with Chicken', 'Lobster and Steak', 'Fish and Chips']
+	'Dessert' => ['Gelato', 'Lava Cake', 'Giant Cookie', 'Cherry Pie']
+	}
+'Location Three' => {
+	'Appetizers' => ['Calmari', 'Tortilla Chips', 'Mini Lamb Sliders']
+	'Salads' => [ 'Caesar', 'Spinach', 'BBQ Chicken', 'Potato']
+	'Main Dishes' => [ 'Kobe Beef Burger', 'Pesto Pasta with Chicken', 'Shrimp and Steak', 'Fish and Chips']
+	'Dessert' => ['Frozen Yogurt', 'Lava Cake', 'Giant Cookie', 'Apple Pie']
+	}
+}
