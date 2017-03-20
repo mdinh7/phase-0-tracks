eca4ef5db8bfe850e161b03b6fc5ac241baee9d2
Mendel Dinh <mdinh6@gmail.com>
2017-03-20 06:45:15 -0700

    Add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,9 @@
+#Pseudocode
+#Ask user for data
+#Convert data into correct data type
+#Store data into hash under appropriate key
+#Ask if key update is needed
+#Update value if updated
+#Break if none is needed
+#Print hash
+#Exit
