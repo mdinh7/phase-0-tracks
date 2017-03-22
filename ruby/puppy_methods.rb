6f4b01e76e8719624d64f23f54d90e435a6234c1
Mendel Dinh <mdinh6@gmail.com>
2017-03-22 15:08:14 -0700

    Add loop, store in array, run instance methods on each loop

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,4 +1,11 @@
+#Partner Omid Fakiri
+
+
 class Puppy
+
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
@@ -33,9 +40,45 @@ class Puppy
   end
 end
 
+
+class News
+
+	def initialize
+		puts "On tonight's news..."
+	end
+
+	def local(time, stories)
+		puts "These are tonight's top #{stories} stories, at #{time}"
+	end
+
+	def emergency(warning)
+		if warning.include?("!")
+			puts warning.upcase
+		else
+			puts warning
+		end
+	end
+
+end
+
 puppy = Puppy.new
 puts puppy.fetch("ball")
 puts puppy.speak(5)
 puts puppy.roll_over
 puts puppy.dog_years(24)
 puts puppy.trick(5, "bone")
+
+index = 0
+while index < 50
+	news = News.new
+	archive = []
+	archive << news
+	archive.each do 
+		news.local(8,4)
+		news.emergency("fire!")
+		end
+	index += 1
+end
+
+archive
+
