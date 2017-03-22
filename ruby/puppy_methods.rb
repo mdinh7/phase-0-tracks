1e52db8dc8fac66ffd4c35b1ea88161d99812341
Mendel Dinh <mdinh6@gmail.com>
2017-03-22 14:27:50 -0700

    Add roll over and speak methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -6,9 +6,16 @@ class Puppy
   end
 
   def speak(bark)
-  	bark.times puts "Woof!"
+  	bark.times do p "Woof!" end
   end
+
+  def roll_over
+  	puts "rolls over"
+  end
+
 end
 
 puppy = Puppy.new
-puts puppy.fetch("bone")
+puts puppy.fetch("ball")
+puts puppy.speak(5)
+puts puppy.roll_over
