4d343139502df3f7338c35b1a649bd465b2635c8
Mendel Dinh <mdinh6@gmail.com>
2017-03-23 10:09:47 -0700

    Add puppy class with behavior and characteristics

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+SPECIES ------------------------
+Shiba Inu
+
+CHARACTERISTICS ----------------
+Meme friendly: yes
+Loveable: yes
+Eye count: 2
+Name: varies
+Softness: varies
+
+BEHAVIOR -----------------------
+Fetch
+Roll
+Eat
