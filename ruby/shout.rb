ff8d21aade576dfc69d9de7be9ccf59ee7fd9a0a
Mendel Dinh <mdinh6@gmail.com>
2017-03-27 04:15:40 -0700

    Add new shout module, add two new classes and driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
   # we'll put some methods here soon, but this code is fine for now!
    def self.yell_angrily(words)
@@ -12,4 +13,25 @@ end
 
 p Shout.yell_happily("potato")
 p Shout.yell_angrily("potato")
+=end
+
+module Shout
+  def yell_lazily(words)
+    puts words.swapcase + "?!" + " why now? (´~`) "
+  end
+end
+
+class Sleepy
+	include Shout
+end
+
+class MorningBoss
+	include Shout
+end
+
+sleepy = Sleepy.new
+sleepy.yell_lazily("WHY DO I HAVE TO WAKE UP")
+
+morning_boss = MorningBoss.new
+morning_boss.yell_lazily("dO i hAvE To dO EvErYthInG")
 
