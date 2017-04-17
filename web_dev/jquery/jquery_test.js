e464cb18eb1d144c05b3dc0fbb78bc7ac0d581dd
Mendel Dinh <mdinh6@gmail.com>
2017-04-17 00:16:34 -0700

    Add test jquery

---
+++ web_dev/jquery/jquery_test.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+	$("button").click(function(){
+        $("#Header2").hide();
+    });
+});
+
+$(document).ready(function(){
+	    $("p").dblclick(function(){
+	    $(this).hide();
+	});
+});
